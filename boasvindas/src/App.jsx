0a844b074beb2aee9bddc9084af7db16af048d79
felipefreitas019 <fefreitassouza@gmail.com>
2025-04-24 11:06:04 -0300

    24/04
    primeira aula de react

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,21 @@
+import BoasVindas from "./exemplo1"
+
+
+function app(){
+
+  return(
+ <div>
+  <h1> Boas vindas ao react com java </h1>
+
+<p> este arquivo app.jsx que sera nosso pornto de partida para o desenvolvimento de aplicação de ract utilizando javascript</p>
+ 
+  <BoasVindas nome = "Felipe"/>
+  <BoasVindas nome = "joão"/>
+  <BoasVindas nome = "miguel"/>
+  <BoasVindas nome = "ruan"/>
+ </div>
+
+  )
+}
+
+export default app
---
+++ boasvindas/src/exemplo1.jsx
@@ -0,0 +1,12 @@
+export default function BoasVindas({nome})
+{
+ return(
+  <div>
+    <p>
+        olá {nome} seja bem vindo ao react
+    </p>
+  </div>
+
+ )
+
+}
